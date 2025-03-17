7a164484c51424c2e784f2c0a2dd131fc79f6077
admin <volkov_154@list.ru>
2025-03-17 17:48:10 +0300

    добавил по todo

--- telegram_bot.py
+++ telegram_bot.py
@@ -17,6 +17,38 @@ from aiohttp import web
 
 router = Router()
 
+# Функция для установки командного меню для бота
+async def set_commands():
+    # Создаем список команд, которые будут доступны пользователям
+    commands = [BotCommand(command='start', description='Старт')]
+    # Устанавливаем эти команды как дефолтные для всех пользователей
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
+
+# Функция, которая будет вызвана при запуске бота
+async def on_startup() -> None:
+    # Устанавливаем командное меню
+    await set_commands()
+    # Устанавливаем вебхук для приема сообщений через заданный URL
+    await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
+    # Отправляем сообщение администратору о том, что бот был запущен
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
+
+
+# Функция, которая будет вызвана при остановке бота
+async def on_shutdown() -> None:
+    # Отправляем сообщение администратору о том, что бот был остановлен
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот остановлен!')
+    # Удаляем вебхук и, при необходимости, очищаем ожидающие обновления
+    await bot.delete_webhook(drop_pending_updates=True)
+    # Закрываем сессию бота, освобождая ресурсы
+    await bot.session.close()
+
+
+
+
+    
+
 # Настройка логгера
 logging.basicConfig(
     level=logging.INFO,
@@ -161,14 +193,17 @@ async def main():
     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
 
-    # Запуск веб-сервера
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, WEB_SERVER_HOST, WEB_SERVER_PORT)
-    await site.start()
+    # Запускаем веб-сервер на указанном хосте и порте (из интернета)
+    web.run_app(app, host=HOST, port=PORT)
 
-    # Установка вебхука
-    await on_startup(bot)
+    # # Запуск веб-сервера
+    # runner = web.AppRunner(app)
+    # await runner.setup()
+    # site = web.TCPSite(runner, WEB_SERVER_HOST, WEB_SERVER_PORT)
+    # await site.start()
+
+    # # Установка вебхука
+    # await on_startup(bot)
 
     logger.info(f"Bot started on {WEB_SERVER_HOST}:{WEB_SERVER_PORT}")
     await asyncio.Event().wait()  # Бесконечное ожидание
